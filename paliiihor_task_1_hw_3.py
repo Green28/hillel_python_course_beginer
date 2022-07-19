7b9cbe232294eece43654b1334fc306b04456376
Ihor <109299012+Green28@users.noreply.github.com>
2022-07-19 16:36:37 +0300

    Update paliiihor_task_1_hw_3.py

--- paliiihor_task_1_hw_3.py
+++ paliiihor_task_1_hw_3.py
@@ -7,8 +7,10 @@
      d)Если больше 21 вывести “You should visit Vietnam.”
      e)Все остальные варианты. Вывести “Travell everywhere”"""
 
+from datetime import date
+
 data_user = input(" Еnter the year of birth: \t")
-data_today: int = 2022
+data_today: int = int(date.today().year)
 if data_user.isdigit():
     age_user = data_today - int(data_user)
     if age_user == 21:
@@ -20,6 +22,6 @@ if data_user.isdigit():
 else:
     print("Incorrect, write a number")
 
-""" Не використовував age: int = int(input()), так як не спрацьовує метод .digit , і якщо намагатись вписати слова або
+""" Не використовував data_user: int = int(input()), так як не спрацьовує метод .digit , і якщо намагатись вписати слова або
  літери , при age: int = int(input()), вилітає помилка невірного ввода типа данних, а нам потрібно дати можливість на 
  введення слів або літер, тому якось так."""
