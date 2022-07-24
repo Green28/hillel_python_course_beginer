65a6ffc4ced152e3fe5d9fea8cd62795a6d2de88
Ihor <109299012+Green28@users.noreply.github.com>
2022-07-24 10:38:13 +0300

    Update paliiihor_task_base_part_hw_3 .py

--- paliiihor_task_base_part_hw_3 .py
+++ paliiihor_task_base_part_hw_3 .py
@@ -1,37 +1,47 @@
 """1_Создать файл (модуль) с примерами всех методов строк."""
 exemple_list: list = ['23', '245', '33']
 exemple_str: str = "Hello my dear friend"
+    
 #  join
 print(f"\n**join** \n{'$'.join(exemple_list)}")
 #   upper,lower, swapcase, capitalize
+
 print(
     "\n**upper,lower, swapcase, capitalize**"
     f"\n{exemple_str.upper()} \n{exemple_str.lower()} \n{exemple_str.swapcase()} \n{exemple_str.capitalize()} ")
 #   count
 print(f"\n**count** \n{exemple_str.count('e')}")
+
 #   find
 print(f"\n**find** \n{exemple_str.find('my')} \n{exemple_str[exemple_str.find('my')]} "
       f"\n{exemple_str[exemple_str.find('my')::]}")
+
 #   replace
 print(f"\n**replace** \n{exemple_str.replace('l', '&', 1)}")
+
 #   strip
 print(f"\n**strip** \n{exemple_str.strip('Hello,friend')}")
+
 #   slit
 print(f"\n**split** \n{exemple_str.split()}")
 
 """2_Создать по 3 варинта  всех уже изученных  объектов в Пайтоне."""
+
 # str
 ex_str1: str = "Whatever your specialty may be, we're here to guide you along the way."
 ex_str2: str = "Hello world of python"
 ex_str3: str = "User_32423"
+
 # int
 ex_int1: int = -23123123
 ex_int2: int = 28
 ex_int3: float = 2.3232301
+
 # list
 ex_list1: list = [1, 2, 3, 4, 5]
 ex_list2: list = [1, 'a', 2, 'b', 3, 'c']
 ex_list3: list = [1, 'a', 2, [1, 4, 5], ('ip', 'id', 'ios'), {'name': 'London'}]
+
 # dict
 ex_dict1: dict = {'user': 'Ivan', 'password': 'qwerty'}
 ex_dict2: dict = {'1': '2', '23': '323'}
@@ -55,6 +65,7 @@ print("may be" in ex_str1)
 
 """6_написать 3 примера условия if elif else"""
 print("\nTask1")
+
 age: int = 19
 if 75 > age >= 18:
     print("Pass free")
@@ -64,6 +75,7 @@ else:
     print("So young")
 print("Task2")
 car_model: str = "BMW"
+
 if car_model == "BMW" or "Mersedes":
     print("You are lucky , it's premium car")
 elif car_model == "VAZ" or "UAZ" or "Moskvich":
@@ -72,6 +84,7 @@ else:
     print("You are lucky but not like owners BMW or Mersedes")
 print("\nTask3")
 friends: int = 4
+    
 if 0 < friends <= 4:
     print("Its OK")
 elif friends > 4:
