1b80313d4d82982b9e41bbcec45be780b18c6180
paliiihor <109299012+Green28@users.noreply.github.com>
2022-08-30 17:04:59 +0300

    Update HW_paliiihor_12.py

--- HW_paliiihor_12.py
+++ HW_paliiihor_12.py
@@ -1,4 +1,5 @@
-from functools import reduce
+import string
+
 
 """Задача 1 10 баллов:
 Написать 3 примера генераторных функций с различными последовательностями."""
@@ -59,38 +60,67 @@ print(next(g3))
 def my_reduce(): моя реализация. (постарайтесь по памяти реализовать.)"""
 
 
+def my_reduce(function, iterable, initializer=None):
+    it = iter(iterable)  # Перетворюєм ітерабельний об'єкт в ітератор
+    if initializer is None:  # Якщо initializer відсутній
+        value = next(it)  # Бере перший об'єкт value запускаєм ітератор
+    else:
+        value = initializer  # #Якщо initializer присутній то перезначаем його і бере його як перший обʼєкт
+    for element in it:  # Перебирає елементи ітератора
+        value = function(value, element)  # Перезначає value викликом атрибутної функції , та приймає в аргумент value
+        # ,і element .
+    return value
+
+
 def funk_reduce(a, b):
     return (a + b) ** b
 
 
-progression = reduce(funk_reduce, (1, 2, 3, 4, 5, 6))
+progression = my_reduce(funk_reduce, (1, 2, 3, 4, 5, 6))
 print(progression)
 
 factor_six = (1, 2, 3, 4, 5, 6)
-factor = reduce(lambda a, b: a * b, factor_six)
+factor = my_reduce(lambda a, b: a * b, factor_six)
 print(factor)
 
-user = reduce(lambda a, b: a.upper() + b, ("p", "a", "l", "i", "i"), "ihor ")
+user = my_reduce(lambda a, b: a.upper() + b, ("p", "a", "l", "i", "i"), "ihor ")
 print(user)
 
 """Задача 3 30 баллов:
 Написать функцию которая с помощью assert будет тестировать ваш самопистный reduce"""
 
 
-def x2(*args):
-    assert len(
-        args) >= 5, "Слишком мало аргументов!"  # Операнд > не в ту сторону повернут у вас бЬIл, для срабатования логики, assert вЬIзЬIвается когда False?
-    return args[4]
+def funk_password(value: str) -> str:
+    digits = string.digits
+    lowers = string.ascii_lowercase
+    uppers = lowers.upper()
+    if len(value) < 8:
+        return "Too Weak"
+    if all(e in digits for e in value) or all(e in lowers for e in value) or all(e in uppers for e in value):
+        return "Weak"
+    if any(e in digits for e in value) and any(e in lowers for e in value) and any(e in uppers for e in value):
+        return "Very Good"
+    if (any(e in digits for e in value) and any(e in lowers for e in value)) or (any(e in lowers for e in value) and
+                                                                                 any(e in uppers for e in value)) or (
+            any(e in digits for e in value) and
+            any(e in uppers for e in value)):
+        return "Good"
 
 
-def funk_reduce(a, b):
-    assert isinstance(a, int) and isinstance(b, int), " Только int"
-    return (a + b) ** b
-
-
-# print(x2(1,2,3))
-# progression2 = reduce(funk_reduce, ("p", "a", "l", "i", "i"))
-# print(progression2)
+if __name__ == '__main__':
+    assert funk_password('') == "Too Weak"
+    assert funk_password('1234567') == "Too Weak"
+    assert funk_password('QWdafe') == "Too Weak"
+    assert funk_password('qwe12') == "Too Weak"
+    assert funk_password('QWERTYU') == "Too Weak"
+    assert funk_password('123456789') == "Weak"
+    assert funk_password('asdfghjkl') == "Weak"
+    assert funk_password('ASDFGHJKL') == "Weak"
+    assert funk_password('qddffdgdgfgd') == "Weak"
+    assert funk_password('1234qwer') == "Good"
+    assert funk_password('qwer1234') == "Good"
+    assert funk_password('12QWqweuj') == "Very Good"
+    assert funk_password('poIU123JL') == "Very Good"
 
 """**Задача 4. 30 баллов:** 
 Збудувати класс з методом котрий повертає всі атрибути єкземпляра класса.
@@ -105,12 +135,12 @@ class Human:
         self.color = color
         self.spesial = special
 
+    def get(self):
+        return self.__dict__
+
 
 adam = Human(25, 'male', 'white', 'protect')
 eva = Human(23, 'female', 'black', 'cook')
-print(eva.__dict__)
-print(adam.__dict__)
+print(eva.get())
+print(adam.get())
 
-"""Задача 5. 20 баллов. Пройти тест
-https://holypython.com/advanced-python-exercises/exercise-4-classes/
-Exercise 4-c: Jet Fighter Instances  -> Прошол"""
